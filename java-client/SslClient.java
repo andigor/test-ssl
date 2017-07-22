619a57aca2056a0dbf39e97dad9cd546dfaa68e7
andigor <amotsok@gmail.com>
2017-07-22 13:48:16 +0200

    something dummy ith java

--- java-client/SslClient.java
+++ java-client/SslClient.java
@@ -1,9 +1,22 @@
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class SslClient
 {
   private static String targetURL = new String("https://127.0.0.1:1111");
 
   public static void main(String[] args)
   {
+    try {
+      URL url = new URL(targetURL);
+      HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
 
+      System.out.println("the end");
+    }
+    catch (Exception e)
+    {
+      System.out.println(e.getMessage());
+    }
   }
 }
